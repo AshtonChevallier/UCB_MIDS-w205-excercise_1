dc7d078bb63860d316ecc2ab456de6163b3126eb
w205 <w205@ip-172-31-14-73.ec2.internal>
2016-10-10 06:31:39 +0000

    Final

--- investigation/best_states/states.sql
+++ investigation/best_states/states.sql
@@ -1,13 +1,19 @@
+
+drop table best_states;
+create table best_states AS
 SELECT 
        State,
-       AVG(Normalized_Score) AS AVG_SCORE,
-       SUM(Score) as Aggregate_Score,
+       AVG(Score)*ln(SUM(Score)) / VARIANCE(Score) as META_SCORE,
+       AVG(Normalized_Score) AS AVG_Normalized_SCORE,
+       AVG(Score) as AVG_Score,
+       ln(SUM(Score)) as Aggregate_Score,
        VARIANCE(Score) as Variability
 from procedures p
 join hospitals h
 on p.provider_id = h.provider_id
 join normalized_scores ns
 on p.provider_id = ns.provider_id
-Group by State
-Order by Avg_score DESC
-limit 10;
+Group by state
+Order by META_SCORE DESC;
+
+select * from best_states limit 10;
--- investigation/hospitals_and_patients/patient.sql
+++ investigation/hospitals_and_patients/patient.sql
@@ -8,27 +8,13 @@ WHERE question <> 'HCAHPS_Base_Score '
 GROUP BY PROVIDER_ID
 ORDER BY AVG_survey DESC;
 
-CREATE TEMPORARY TABLE Max_Scores
-AS SELECT
-   Measure_id,
-   Max(Score) as Max_Score
-FROM procedures
-GROUP By Measure_id;
 
-CREATE TEMPORARY TABLE Normalized_SCores
-AS SELECT 
-       Provider_ID,
-       p.Measure_ID,
-       (Score/Max_Score) as Normalized_Score
-From procedures p
-left join Max_Scores ms
-on p.measure_id = ms.measure_id;
 
 
 SELECT
-	corr(Normalized_Score, AVG_survey) as COV
+	corr(Normalized_Score, AVG_survey) as COr
 from avg_survey a
-join Normalized_SCores ns
+join Normalized_SCores_2 ns
 on a.provider_id = ns.provider_id
-Order by COV desc
+Order by COR desc
 limit 10;
