c26fc1a7874301f0ecdc798f75fdb4cb1e7acddd
w205 <w205@ip-172-31-14-73.ec2.internal>
2016-10-10 03:41:15 +0000

    Almost done

--- investigation/best_hospitals/high_quality_hospitals.sql
+++ investigation/best_hospitals/high_quality_hospitals.sql
@@ -16,11 +16,13 @@ From procedures p
 left join Max_Scores ms
 on p.measure_id = ms.measure_id;	
  
-drop table  average_scores;
-
+drop table best_hospitals;
+create table best_hospitals AS
 SELECT 
 	Name,
-	AVG(Normalized_Score) AS AVG_SCORE,
+	AVG(Normalized_Score)*SUM(Score) - VARIANCE(Score) as META_SCORE,
+	AVG(Normalized_Score) AS AVG_Normalized_SCORE,
+	AVG(Score) as AVG_Score,
 	SUM(Score) as Aggregate_Score,
 	VARIANCE(Score) as Variability
 from procedures p
@@ -29,6 +31,7 @@ on p.provider_id = h.provider_id
 join normalized_scores ns
 on p.provider_id = ns.provider_id
 Group by Name
-Order by Aggregate_Score DESC
-limit 10;
+Order by META_SCORE DESC;
+
+select * from best_hospitals limit 10;
 
--- investigation/hospitals_variability/variability.sql
+++ investigation/hospitals_variability/variability.sql
@@ -1,3 +1,6 @@
+drop TABLE measure_variance;
+CREATE TABLE measure_variance
+as
 select
 Measure_Name,
 VARIANCE(Score) as Variability
@@ -5,5 +8,7 @@ from procedures p
 join measures m
 on p.measure_id = m.measure_id
 group by Measure_Name
-Order by Variability DESC
-Limit 10;
+Order by Variability DESC;
+
+select * from measure_variance 
+limit 10;
